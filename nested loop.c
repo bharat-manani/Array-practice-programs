fe9ae73504a8ca3f1ba3bd70bb3b1cf02056588c
Bharat Manani <k250165@nu.edu.pk>
2025-10-23 09:28:45 +0500

    Add files via upload

---
+++ nested loop.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+ //nested loop
+ //pyramid pattern
+ 
+ printf("---------- Pyramid Pattern of Numbers ------------- \n");
+ int i=0;
+ int j=0;
+
+ 
+ for(i = 1; i<=9; i++) {
+ 	for(j = 0; j<i; j++)
+ 	  
+ 	  	printf("%d  ",i);
+ 	  	printf("\n");
+	   }
+ 	  
+ 	
+ 		
+ 	
+	
+
+	
+	return 0;
+}
